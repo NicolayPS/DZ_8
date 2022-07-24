3df723383604505afb37c9a4259a3a76bc035d89
oksanaPC2 <77opod@gmail.com>
2022-07-24 20:02:10 +0300

    Добавьте файлы проекта.

---
+++ DZ_8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DZ_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Temp";
+            var dirP = @"D:\Temp";
+            var dir = Directory.EnumerateDirectories(dirP, "*", SearchOption.AllDirectories);
+            foreach (var item in dir)
+            {
+                Console.WriteLine(item);
+            }
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            DirectoryInfo Disk = new DirectoryInfo(path);
+            DirectoryInfo[] disk = Disk.GetDirectories();
+            foreach (string file in files)
+            {
+                
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ №3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] textMass;
+            StreamReader sr = new StreamReader(@"D:\Temp\Text.txt");
+            string s = "";
+            while (sr.EndOfStream != true)
+            {
+                s += sr.ReadLine();
+            }
+            textMass = s.Split(' ');
+            Console.Write("Количество слов:");
+            Console.WriteLine(textMass.Length);
+            sr.Close();
+                Console.ReadLine();
+        }
+    }
+
+}
